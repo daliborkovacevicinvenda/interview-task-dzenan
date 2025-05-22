c3188ce8c210a000126901f8f408b68e914d9f16
DzenanB <149873268+DzenanB@users.noreply.github.com>
2025-05-22 19:03:46 +0200

    Added AppDbContext and fixed typo for the Customer model

---
+++ WebShopSimulation/WebShopSimulation/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebShopSimulation.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Shop> Shops { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+}
