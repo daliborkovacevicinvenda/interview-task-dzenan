4f24a8d2bed0c0f8aecdd11ebc64cd725c571a46
DzenanB <149873268+DzenanB@users.noreply.github.com>
2025-05-22 19:26:27 +0200

    Added ShopController with CRUD functionality

---
+++ WebShopSimulation/WebShopSimulation/Controllers/ShopController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebShopSimulation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/shop
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Shop>>> GetShops()
+        {
+            return await _context.Shops.Include(s => s.Products).ToListAsync();
+        }
+
+        // GET: api/shop/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shop>> GetShop(int id)
+        {
+            var shop = await _context.Shops.Include(s => s.Products).FirstOrDefaultAsync(s => s.Id == id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+            return shop;
+        }
+
+        // POST: api/shop
+        [HttpPost]
+        public async Task<ActionResult<Shop>> CreateShop(Shop shop)
+        {
+            _context.Shops.Add(shop);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetShop), new { id = shop.Id }, shop);
+        }
+
+        // PUT: api/shop/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateShop(int id, Shop updatedShop)
+        {
+            if (id != updatedShop.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(updatedShop).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Shops.Any(s => s.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/shop/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteShop(int id)
+        {
+            var shop = await _context.Shops.FindAsync(id);
+            if (shop == null)
+            {
+                return NotFound();
+            }
+
+            _context.Shops.Remove(shop);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
