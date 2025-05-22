43985dece3ff653d1645b672121cb7dff23c9fd5
DzenanB <149873268+DzenanB@users.noreply.github.com>
2025-05-23 01:13:18 +0200

    Added Swagger for testing in Program.cs

--- WebShopSimulation/WebShopSimulation/Program.cs
+++ WebShopSimulation/WebShopSimulation/Program.cs
@@ -2,18 +2,25 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); // Register the API explorer for Swagger
+builder.Services.AddSwaggerGen(); // Register Swagger generator
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Swagger only in Development mode
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwaggerUI();
+}
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
